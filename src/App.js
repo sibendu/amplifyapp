93bf8101ad7026ac46e5046a9ab461d6b94d98b1
Das <Sibendu.Das@philips.com>
2020-09-07 22:59:05 +0530

    added auth

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>We now have Authentication in the app!</h1>
+        <h1>We now have Authentication!</h1>
       </header>
       <AmplifySignOut />
     </div>
